0bfe673b9023cb324510a8ffab667dc0f9647318
Dillon Mulroy <dillon@mainstreet.com>
2022-11-27 21:25:04 -0500

    update remaps

--- .config/nvim/lua/user/remap.lua
+++ .config/nvim/lua/user/remap.lua
@@ -1,6 +1,7 @@
 local nnoremap = require("user.keymap").nnoremap
 local inoremap = require("user.keymap").inoremap
 local vnoremap = require("user.keymap").vnoremap
+local xnoremap = require("user.keymap").xnoremap
 local map = require("user.keymap").map
 
 -- Normal --
@@ -20,15 +21,14 @@ nnoremap("<leader>w", "<cmd>w<cr>", {silent = false})
 -- Quit with leader key
 nnoremap("<leader>q", "<cmd>q<cr>", {silent = false})
 
--- Quit with leader key
-nnoremap("<leader>q", "<cmd>q<cr>", {silent = false})
-
 -- Map netrw to <leader>e
 nnoremap("<leader>e", "<cmd>Lex 20<cr>")
 
--- Center buffer while navigating with <C-u> and <C-d> or when hopping through search results
+-- Center buffer while navigating
 nnoremap("<C-u>", "<C-u>zz")
 nnoremap("<C-d>", "<C-d>zz")
+nnoremap("{", "{zz")
+nnoremap("}", "}zz")
 nnoremap("N", "Nzz")
 nnoremap("n", "nzz")
 
@@ -39,11 +39,16 @@ nnoremap("S", ":%s//g<left><left>", {silent = false})
 nnoremap("<S-L>", "$")
 nnoremap("<S-H>", "^")
 
+-- Turn off highligted results
+nnoremap("<leader>no", "<cmd>noh<cr>")
+
 -- Insert --
 -- Map jj to <esc>
 inoremap("jj", "<esc>")
 
 -- Visual --
+xnoremap("<leader>p", '"_dP')
 -- Move selected text up/down in visual mode (TODO: Figure out how to enable this for multi line) 
 -- vnoremap("<A-k>", "<cmd>m '<-2<CR>gv=gv") 
 -- vnoremap("<A-j>", "<cmd>m '>+1<CR>gv=gv")
+
