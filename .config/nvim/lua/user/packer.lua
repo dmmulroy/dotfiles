86294b7ff349ad8f42a24e27b0e5ec9589ab6b6b
Dillon Mulroy <dillon@mainstreet.com>
2022-12-01 15:46:21 -0500

    update packer file

--- .config/nvim/lua/user/packer.lua
+++ .config/nvim/lua/user/packer.lua
@@ -1,10 +1,30 @@
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local packer_install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
-return require('packer').startup(function(use)
+local is_bootstrapped = false
+
+-- If packer hasn't previously been installed, git clone and install it
+if vim.fn.empty(vim.fn.glob(packer_install_path)) > 0 then
+  is_bootstrapped = true
+  vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. packer_install_path)
+  vim.cmd([[packadd packer.nvim]])
+end
+
+-- Set up an autocmd to automatically run PackerCompile on updates to this file
+vim.api.nvim_create_autocmd('BufWritePost', {
+  command = 'source <afile> | PackerCompile',
+  group = vim.api.nvim_create_augroup("recompile_packer", { clear = true}),
+  pattern = vim.fn.stdpath("config") .. "/lua/user/packer.lua"
+})
+
+
+require("packer").startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use("wbthomason/packer.nvim")
 
   -- Install the catppuccin theme
-  use { "catppuccin/nvim", as = "catppuccin" }
+  use({ "catppuccin/nvim", as = "catppuccin" })
+
+  if is_bootstrapped then
+    require("packer").sync()
+  end
 end)
