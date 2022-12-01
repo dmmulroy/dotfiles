9e2f2bfa2195a17069b26e89319026280c836c43
Dillon Mulroy <dillon@mainstreet.com>
2022-11-30 22:16:21 -0500

    update nvim config

--- .config/nvim/lua/user/packer.lua
+++ .config/nvim/lua/user/packer.lua
@@ -4,5 +4,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
+  -- Install the catppuccin theme
   use { "catppuccin/nvim", as = "catppuccin" }
 end)
