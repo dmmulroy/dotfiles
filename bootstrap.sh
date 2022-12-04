9757106656e4d1ff78975db0d9de87fdcca4ef0f
dmmulroy <dillon.mulroy@gmail.com>
2022-12-03 20:41:52 -0500

    fix typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ ZSH_CUSTOM=$ZSH/custom
 # Clone/install zsh-autoggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# Clone/isntall zsh-syntax-highlighting
+# Clone/install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
