850d173c0ced9527da47bf166fcf2be4ec4e362c
dmmulroy <dillon.mulroy@gmail.com>
2022-12-03 21:04:26 -0500

    update lsp plugins

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -75,3 +75,30 @@ end
 
 -- Turn on LSP status and progress information
 require("fidget").setup()
+
+-- Override default Lua LSP to include neovim globals/runtime
+-- TODO: Move this to another module/file
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+require('lspconfig').sumneko_lua.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = { enable = false },
+    },
+  },
+})
