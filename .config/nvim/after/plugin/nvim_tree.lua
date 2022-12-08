72916905ccc68969a59f481c691191ba867fa294
Dillon Mulroy <dillon.mulroy@gmail.com>
2022-12-07 22:25:56 -0500

    updates

--- .config/nvim/after/plugin/nvim_tree.lua
+++ .config/nvim/after/plugin/nvim_tree.lua
@@ -1,2 +1,5 @@
--- See *nvim-tree-default-mappings* for default kep binds
-require("nvim-tree").setup({})
+-- Disable netrw to prevent race conditions w/ nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup({}) -- See *nvim-tree-default-mappings* for default kep binds
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,1 @@
--- Disable netrw to prevent race conditions w/ nvim-tree
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 require("user")
