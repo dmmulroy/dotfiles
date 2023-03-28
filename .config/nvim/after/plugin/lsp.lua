968b9aa611ba746c55fce2d117179ba4193c03c9
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-03-28 18:37:16 -0400

    update nvim conf

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -4,36 +4,56 @@ local map_lsp_keybinds = require("user.keymaps").map_lsp_keybinds
 -- Use neodev to configure lua_ls in nvim directories - must load before lspconfig
 require("neodev").setup()
 
+-- Override tsserver diagnostics to filter out specific messages
+local function tsserver_on_publish_diagnostics_override(_, result, ctx, config)
+  local filtered_diagnostics = {}
+
+  for _, diagnostic in ipairs(result.diagnostics) do
+    if not string.find(diagnostic.message, "This may be converted to an async function") then
+      table.insert(filtered_diagnostics, diagnostic)
+    end
+  end
+
+  result.diagnostics = filtered_diagnostics
+
+  vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+end
+
 -- LSP servers to install (see list here: https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers)
 local servers = {
-	tsserver = {
-		experimental = {
-			enableProjectDiagnostics = true,
-		},
-	},
-	cssls = {},
-	graphql = {},
-	jsonls = {},
-	marksman = {},
-	prismals = {},
-	sqlls = {},
-	tailwindcss = {},
-	bashls = {},
-	yamlls = {},
-	lua_ls = {},
-	ocamllsp = {},
+  tsserver = {
+    settings = {
+      experimental = {
+        enableProjectDiagnostics = true,
+      },
+    },
+    handlers = {
+      ["textDocument/publishDiagnostics"] = vim.lsp.with(tsserver_on_publish_diagnostics_override, {}),
+    },
+  },
+  cssls = {},
+  graphql = {},
+  jsonls = {},
+  marksman = {},
+  prismals = {},
+  sqlls = {},
+  tailwindcss = {},
+  bashls = {},
+  yamlls = {},
+  lua_ls = {},
+  ocamllsp = {},
 }
 
 -- Setup mason so it can manage 3rd party LSP servers
 require("mason").setup({
-	ui = {
-		border = "rounded",
-	},
+  ui = {
+    border = "rounded",
+  },
 })
 
 -- Configure mason to auto install servers
 require("mason-lspconfig").setup({
-	automatic_installation = { exclude = { "ocamllsp" } },
+  automatic_installation = { exclude = { "ocamllsp" } },
 })
 
 -- nvim-cmp supports additional completion capabilities
@@ -41,27 +61,28 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local on_attach = function(_, buffer_number)
-	-- Pass the current buffer to map lsp keybinds
-	map_lsp_keybinds(buffer_number)
+  -- Pass the current buffer to map lsp keybinds
+  map_lsp_keybinds(buffer_number)
 
-	-- Create a command `:Format` local to the LSP buffer
-	vim.api.nvim_buf_create_user_command(buffer_number, "Format", function(_)
-		vim.lsp.buf.format({
-			filter = function(client)
-				-- Use Prettier to format TS/JS if it's available
-				return client.name ~= "tsserver" or not null_ls.is_registered("prettier")
-			end,
-		})
-	end, { desc = "LSP: Format current buffer with LSP" })
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(buffer_number, "Format", function(_)
+    vim.lsp.buf.format({
+      filter = function(client)
+        -- Use Prettier to format TS/JS if it's available
+        return client.name ~= "tsserver" or not null_ls.is_registered("prettier")
+      end,
+    })
+  end, { desc = "LSP: Format current buffer with LSP" })
 end
 
 -- Iterate over our servers and set them up
-for name, settings in pairs(servers) do
-	require("lspconfig")[name].setup({
-		on_attach = on_attach,
-		capabilities = capabilities,
-		settings = settings,
-	})
+for name, config in pairs(servers) do
+  require("lspconfig")[name].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = config.settings,
+    handlers = config.handlers,
+  })
 end
 
 -- Congifure LSP linting/formatting
@@ -70,27 +91,27 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
-	border = "rounded",
-	sources = {
-		-- formatting
-		formatting.prettier,
-		formatting.stylua,
-		formatting.ocamlformat,
+  border = "rounded",
+  sources = {
+    -- formatting
+    formatting.prettier,
+    formatting.stylua,
+    formatting.ocamlformat,
 
-		-- diagnostics
-		diagnostics.eslint_d.with({
-			condition = function(utils)
-				return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" })
-			end,
-		}),
+    -- diagnostics
+    diagnostics.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" })
+      end,
+    }),
 
-		-- code actions
-		code_actions.eslint_d.with({
-			condition = function(utils)
-				return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" })
-			end,
-		}),
-	},
+    -- code actions
+    code_actions.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" })
+      end,
+    }),
+  },
 })
 
 -- Turn on LSP status and progress information
--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,5 +1,22 @@
+local function truncate_branch_name(branch)
+	if not branch or branch == "" then
+		return ""
+	end
+
+	-- Match the branch name to the specified format
+	local _, _, ticket_number = string.find(branch, "skdillon/sko%-(%d+)%-")
+
+	-- If the branch name matches the format, display sko-{ticket_number}, otherwise display the full branch name
+	if ticket_number then
+		return "sko-" .. ticket_number
+	else
+		return branch
+	end
+end
+
 require("lualine").setup({
 	sections = {
+		lualine_b = { { "branch", icon = "", fmt = truncate_branch_name }, "diff", "diagnostics" },
 		lualine_c = {
 			{ "filename", path = 1 },
 		},
