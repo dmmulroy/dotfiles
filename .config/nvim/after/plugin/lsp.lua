a26d936d83841f8d3226e62d17cc8e63dddc1571
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-04-16 17:36:19 -0400

    update nvim conf

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -5,55 +5,83 @@ local map_lsp_keybinds = require("user.keymaps").map_lsp_keybinds
 require("neodev").setup()
 
 -- Override tsserver diagnostics to filter out specific messages
+local messages_to_filter = {
+	"This may be converted to an async function",
+	"'_Assertion' is declared but never used.",
+}
+
 local function tsserver_on_publish_diagnostics_override(_, result, ctx, config)
-  local filtered_diagnostics = {}
+	local filtered_diagnostics = {}
 
-  for _, diagnostic in ipairs(result.diagnostics) do
-    if not string.find(diagnostic.message, "This may be converted to an async function") then
-      table.insert(filtered_diagnostics, diagnostic)
-    end
-  end
+	for _, diagnostic in ipairs(result.diagnostics) do
+		local found = false
+		for _, message in ipairs(messages_to_filter) do
+			if string.find(diagnostic.message, message) then
+				found = true
+				break
+			end
+		end
+		if not found then
+			table.insert(filtered_diagnostics, diagnostic)
+		end
+	end
 
-  result.diagnostics = filtered_diagnostics
+	result.diagnostics = filtered_diagnostics
 
-  vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+	vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
+end
+
+-- Override ocamllsp cmd if it's in the current project
+local function ocamllsp_cmd_override()
+	local root = vim.fn.getcwd()
+	local ocamllsp_binary = root .. "/_opam/bin/ocamllsp"
+
+	if vim.fn.executable(ocamllsp_binary) == 1 then
+		return { ocamllsp_binary }
+	else
+		return { "ocamllsp" }
+	end
 end
 
 -- LSP servers to install (see list here: https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers)
 local servers = {
-  tsserver = {
-    settings = {
-      experimental = {
-        enableProjectDiagnostics = true,
-      },
-    },
-    handlers = {
-      ["textDocument/publishDiagnostics"] = vim.lsp.with(tsserver_on_publish_diagnostics_override, {}),
-    },
-  },
-  cssls = {},
-  graphql = {},
-  jsonls = {},
-  marksman = {},
-  prismals = {},
-  sqlls = {},
-  tailwindcss = {},
-  bashls = {},
-  yamlls = {},
-  lua_ls = {},
-  ocamllsp = {},
+	tsserver = {
+		settings = {
+			experimental = {
+				enableProjectDiagnostics = true,
+			},
+		},
+		handlers = {
+			["textDocument/publishDiagnostics"] = vim.lsp.with(tsserver_on_publish_diagnostics_override, {}),
+		},
+	},
+	cssls = {},
+	graphql = {},
+	jsonls = {},
+	marksman = {},
+	prismals = {},
+	sqlls = {},
+	tailwindcss = {},
+	bashls = {},
+	yamlls = {},
+	lua_ls = {},
+	ocamllsp = {
+		-- settings = {
+		-- 	cmd = ocamllsp_cmd_override(),
+		-- },
+	},
 }
 
 -- Setup mason so it can manage 3rd party LSP servers
 require("mason").setup({
-  ui = {
-    border = "rounded",
-  },
+	ui = {
+		border = "rounded",
+	},
 })
 
 -- Configure mason to auto install servers
 require("mason-lspconfig").setup({
-  automatic_installation = { exclude = { "ocamllsp" } },
+	automatic_installation = { exclude = { "ocamllsp" } },
 })
 
 -- nvim-cmp supports additional completion capabilities
@@ -61,28 +89,28 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local on_attach = function(_, buffer_number)
-  -- Pass the current buffer to map lsp keybinds
-  map_lsp_keybinds(buffer_number)
+	-- Pass the current buffer to map lsp keybinds
+	map_lsp_keybinds(buffer_number)
 
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(buffer_number, "Format", function(_)
-    vim.lsp.buf.format({
-      filter = function(client)
-        -- Use Prettier to format TS/JS if it's available
-        return client.name ~= "tsserver" or not null_ls.is_registered("prettier")
-      end,
-    })
-  end, { desc = "LSP: Format current buffer with LSP" })
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(buffer_number, "Format", function(_)
+		vim.lsp.buf.format({
+			filter = function(client)
+				-- Use Prettier to format TS/JS if it's available
+				return client.name ~= "tsserver" or not null_ls.is_registered("prettier")
+			end,
+		})
+	end, { desc = "LSP: Format current buffer with LSP" })
 end
 
 -- Iterate over our servers and set them up
 for name, config in pairs(servers) do
-  require("lspconfig")[name].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = config.settings,
-    handlers = config.handlers,
-  })
+	require("lspconfig")[name].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		settings = config.settings,
+		handlers = config.handlers,
+	})
 end
 
 -- Congifure LSP linting/formatting
@@ -91,27 +119,27 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
-  border = "rounded",
-  sources = {
-    -- formatting
-    formatting.prettier,
-    formatting.stylua,
-    formatting.ocamlformat,
+	border = "rounded",
+	sources = {
+		-- formatting
+		formatting.prettier,
+		formatting.stylua,
+		formatting.ocamlformat,
 
-    -- diagnostics
-    diagnostics.eslint_d.with({
-      condition = function(utils)
-        return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" })
-      end,
-    }),
+		-- diagnostics
+		diagnostics.eslint_d.with({
+			condition = function(utils)
+				return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" })
+			end,
+		}),
 
-    -- code actions
-    code_actions.eslint_d.with({
-      condition = function(utils)
-        return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" })
-      end,
-    }),
-  },
+		-- code actions
+		code_actions.eslint_d.with({
+			condition = function(utils)
+				return utils.root_has_file({ ".eslintrc.js", ".eslintrc.cjs", ".eslintrc.json" })
+			end,
+		}),
+	},
 })
 
 -- Turn on LSP status and progress information
