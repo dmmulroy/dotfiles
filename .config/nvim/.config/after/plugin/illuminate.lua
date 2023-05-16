f514e5061cfa53673cafe9a310a0a13775ca80b9
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-05-15 23:20:07 -0400

    nvim updats

---
+++ .config/nvim/.config/after/plugin/illuminate.lua
@@ -0,0 +1,11 @@
+require("illuminate").configure({
+	under_cursor = false,
+	filetypes_denylist = {
+		"alpha",
+		"NvimTree",
+		"Outline",
+		"toggleterm",
+		"DressingSelect",
+		"TelescopePrompt",
+	},
+})
